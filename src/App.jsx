9f1bd3f4b688a3d859f4c8e0df81056899c8ee78
tanmayc31 <tanmaychandan31@gmail.com>
2025-08-12 13:01:37 -0400

    created common component - ProgressBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import HeaderWithUser from './components/common/Header/HeaderWithUser'
-
+// import HeaderWithoutUser from './components/common/Header/HeaderWithoutUser'
+import ProgressBar from './components/common/ProgressBar/ProgressBar';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       {/* <HeaderWithoutUser /> */}
 
       <HeaderWithUser />
+      <ProgressBar />
     </div>
   )
 }
---
+++ src/components/common/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styles from './ProgressBar.module.css';
+
+const ProgressBar = ({ currentStep = 1 }) => {
+    const stepTitles = [
+        'New DNV Quote Request',
+        'Facility Details',
+        'Leadership Contacts',
+        'Site Information',
+        'Services & Certifications',
+        'Review & Submit'
+    ];
+
+    const stepLabels = [
+        'DNV Quote Request',
+        'Facility Details',
+        'Leadership Contacts',
+        'Site Information',
+        'Services & Certifications',
+        'Review & Submit'
+    ];
+
+    return (
+        <div className={styles.progressBar}>
+            {/* Title Section */}
+            <div className={styles.titleSection}>
+                <div className={styles.container}>
+                    <h2 className={styles.title}>{stepTitles[currentStep - 1]}</h2>
+                    <span className={styles.stepText}>Step {currentStep} of 6</span>
+                </div>
+            </div>
+
+            {/* Progress Lines */}
+            <div className={styles.progressSection}>
+                <div className={styles.container}>
+                    <div className={styles.progressLines}>
+                        {stepLabels.map((label, index) => {
+                            const stepNumber = index + 1;
+                            const isCompleted = stepNumber < currentStep;
+                            const isCurrent = stepNumber === currentStep;
+                            const isInactive = stepNumber > currentStep;
+
+                            return (
+                                <div key={index} className={styles.stepContainer}>
+                                    <div className={styles.progressLineWrapper}>
+                                        <div className={`${styles.progressLineBackground} ${isCompleted ? styles.completed :
+                                                isCurrent ? styles.current :
+                                                    styles.inactive
+                                            }`}></div>
+                                        {isCurrent && (
+                                            <div className={styles.progressLineActive}></div>
+                                        )}
+                                    </div>
+                                    <span className={styles.stepLabel}>{label}</span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProgressBar;
